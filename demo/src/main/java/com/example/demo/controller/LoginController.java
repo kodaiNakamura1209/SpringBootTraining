59e1c5bedc92d4b97210da673d69c26189a125da
kodaiNakamura1209 <natural.green1209@ezweb.ne.jp>
2023-06-05 18:28:43 +0900

    入力フォームの値を参照し、DBの情報を取得するようにシステムを改良

--- demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dto.LoginDto;
 import com.example.demo.exception.ValidatedException;
+import com.example.demo.service.LoginService;
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private LoginService service;
 	
     @GetMapping("/login/top")
     public String loginTop(Model model) {
@@ -24,7 +29,7 @@ public class LoginController {
     }
     
     @PostMapping("/login/loginDo")
-    public String loginDo(@Validated @ModelAttribute LoginDto loginDto,  BindingResult bindingResult) {
+    public String loginDo(Model model, @Validated @ModelAttribute LoginDto loginDto,  BindingResult bindingResult) {
     	
     	// エラーチェック
     	if (bindingResult.hasErrors()) {
@@ -33,6 +38,18 @@ public class LoginController {
     		throw new ValidatedException("E_001", bindingResult.getFieldError().getDefaultMessage());
     	}
     	
+    	// ログインユーザーの情報を取得
+    	LoginDto result = service.loginUserSelect(loginDto);
+    	
+    	// ユーザーIDが空の場合はログイン失敗のため、前のページに戻る
+    	if("".equals(loginDto.getUserId())) {
+        	// ログイン情報格納用のオブジェクトを設定
+        	model.addAttribute("loginInfo", new LoginDto());
+    		return "login";
+    	}
+    	
+    	// 情報が格納されている場合は、マイページへ遷移
+    	model.addAttribute("mypageInfo", result);
     	return "myPage";
     }
 }
--- demo/src/main/java/com/example/demo/dto/LoginDto.java
+++ demo/src/main/java/com/example/demo/dto/LoginDto.java
@@ -1,13 +1,17 @@
 package com.example.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
 
 	@NotBlank(message = "{NotBlank.LoginDto.userId}")
@@ -15,4 +19,6 @@ public class LoginDto {
 	
 	@NotBlank(message = "{NotBlank.LoginDto.userPassword}")
 	private String userPassword;
+	
+	private String userName;
 }
